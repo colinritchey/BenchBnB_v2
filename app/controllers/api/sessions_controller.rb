b5ff2d400c896cf53e4d652e230e2807ee8096b6
Colin Ritchey <colinritchey256@gmail.com>
2017-03-10 14:25:06 -0800

    User sign up, sign in and sign out on backend

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,29 @@
+class Api::SessionsController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
+  def create
+    user = User.find_by_credintials(params[:user][:username], params[:user][:password])
+    if user
+      sign_in(user)
+      # render 'api/users/show'
+      render json: {}
+    else
+      render json: {errors: ["Invalid login"], status: 401}
+    end
+  end
+
+  def destroy
+    @user = current_user
+
+    if @user
+      logout
+      render json: {}
+    else
+      render json: {errors: ["No current user"], status: 404}
+    end
+  end
+
+end
